c95f1b4db9b28819d964c3e5c4a7471f404af245
Tanjim Noor <tanjim.noor@g.bracu.ac.bd>
2025-01-04 07:01:34 +0600

    Configuration done. Geolocation hook implemented

---
+++ src/hooks/useGeolocation.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+export default function useGeolocation() {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(true);
+  const [date, setDate] = useState(true);
+
+  useEffect(() => {
+    const onSuccess = (e) => {
+      setLoading(false);
+      setError(null);
+      setDate(e.coords);
+    };
+
+    const onError = (e) => {
+      setLoading(false);
+      setError(e);
+    };
+    navigator.geolocation.getCurrentPosition(onSuccess, onError);
+  }, []);
+}
